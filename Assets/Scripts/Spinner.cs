cbaa4aed9f1c21842694b57b0e164df97620c02a
Punit Tilwani <punittilwani30@gmail.com>
2025-11-02 14:45:26 +0530

    Punit
    Designed Other Levels

---
+++ Assets/Scripts/Spinner.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class Spinner : MonoBehaviour
+{
+    public Vector3 rotateAngles;
+
+    private void Update()
+    {
+        transform.Rotate(rotateAngles);
+    }
+}
